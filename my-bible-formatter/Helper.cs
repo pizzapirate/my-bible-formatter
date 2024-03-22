1c2f12673ea1285c7c974e71364b59ca468012be
Miles Vellozzo <miles.vellozzo@bwhhotels.co.uk>
2024-03-22 13:10:32 +0000

    ready to export to database

--- my-bible-formatter/Helper.cs
+++ my-bible-formatter/Helper.cs
@@ -1,11 +1,14 @@
 ﻿using my_bible_formatter.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace my_bible_formatter
 {
@@ -17,7 +20,42 @@ namespace my_bible_formatter
 
             string[] pieces = jsonBible.Split(new[] { '{', '}' }, StringSplitOptions.RemoveEmptyEntries);
 
-            return null;
+            List<BibleVerse> bibleVerses = [];
+
+            foreach (var piece in pieces)
+            {
+                // Check if the line starts with " \"field\": ["
+                if (!Regex.IsMatch(piece, "^ \"field\": \\["))
+                {
+                    continue;
+                }
+                // Remove the leading " \"field\": [" and trailing "] "
+                string parts = piece.Substring(11, piece.Length - 14);
+
+                // Split the string by comma, but only for the first 4 commas
+                string[] part = parts.Split(new char[] { ',' }, 5);
+
+                // Parse each number and store it as an integer
+                int num1 = int.Parse(part[0]);
+                int book = int.Parse(part[1]);
+                int chapter = int.Parse(part[2]);
+                int verse = int.Parse(part[3]);
+
+                // The remaining part of the string
+                string text = part[4];
+
+                BibleVerse bv = new()
+                {
+                    Text = text,
+                    Book = book,
+                    Chapter = chapter,
+                    Verse = verse
+                };
+
+                bibleVerses.Add(bv);
+            }
+
+            return bibleVerses;
         }
     }
 }
--- my-bible-formatter/Program.cs
+++ my-bible-formatter/Program.cs
@@ -2,8 +2,10 @@
 using my_bible_formatter.Models;
 using System.Reflection;
 
-string pathToGodsWord = @"C:\Users\mvell\source\repos\my-bible-formatter\my-bible-formatter\kjv.json";
+string pathToGodsWord = @"C:\Users\mvellozzo\source\repos\my-bible-formatter\my-bible-formatter\kjv.json";
 
 var bible = Helper.ListifyBible(pathToGodsWord);
+
+
 
 Console.WriteLine(bible);
