55353c55cc790b22aaf334c9e51f7f2fdc1d05cc
BuildTools <unconfigured@null.spigotmc.org>
2024-04-18 19:12:06 +0100

    asd

--- my-bible-formatter/Program.cs
+++ my-bible-formatter/Program.cs
@@ -2,13 +2,13 @@
 using my_bible_formatter.Models;
 using System.Reflection;
 
-string path = @"C:\Users\mvell\source\repos\my-bible-formatter\my-bible-formatter\ylt.xml";
+string path = @"C:\Users\mvell\source\repos\my-bible-formatter\my-bible-formatter\web.xml";
 // https://github.com/seven1m/open-bibles
 //var bible = Helper.KJVListifyBible(pathToGodsWord);
 
 var bible = Helper.XMLListifyBible(path);
 
-Helper.SaveFormattedBible(bible, @"C:\Users\mvell\Downloads\ylt-formatted.json");
+Helper.SaveFormattedBible(bible, @"C:\Users\mvell\Downloads\web-formatted.json");
 
 
 Console.WriteLine("Done.");
